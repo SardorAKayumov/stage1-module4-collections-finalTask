4d027ea2bb0ceeea66cf44c00bfc003c97223408
SardorAKayumov <sardor.zeroz@gmail.com>
2022-11-28 20:27:05 +0500

    commit

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,23 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                list.add(entry.getKey());
+            }
+        }
+
+        list.sort((o1, o2) -> {
+            if(o1.length() == o2.length()) {
+                return o2.compareToIgnoreCase(o1);
+            }
+            return o1.compareToIgnoreCase(o2);
+        });
+
+        return list;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,21 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> map = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            Integer keyLength = entry.getKey().length();
+            map.putIfAbsent(keyLength, new HashSet<>());
+            Set<String> set = new HashSet<>(map.get(keyLength));
+            set.add(entry.getKey());
+            map.put(keyLength, set);
+        }
+
+        return map;
     }
 }
